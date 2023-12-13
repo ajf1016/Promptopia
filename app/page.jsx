3938b86e80e9b22edf303436a10b573a89e6d84b
ajf1016 <talropajmal1016@gmail.com>
2023-12-14 00:47:47 +0530

    file structure

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+      <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      
+      <p className="desc text-center">
+        Promptopia is an open source ai prompting too for modern world descover, create and share creative prompts
+      </p>
+    </section> 
+  )
+}
+
+export default Home
